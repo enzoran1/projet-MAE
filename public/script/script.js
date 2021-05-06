bccb400c1ad533c4b1d840a9269c1a6f315c59fa
Enzo Franceschini <enzo.franceschinidev@gmail.com>
2021-05-06 14:37:13 +0200

    new commit enzo architecture projet

---
+++ public/script/script.js
@@ -0,0 +1,20 @@
+const navButton = document.querySelector(".nav-button");
+const navMenu = document.querySelector(".nav-menu");
+
+
+
+navButton.addEventListener("click", function(){
+    navMenu.classList.toggle("show");
+})
+const navButton2 = document.querySelector(".nav-button2");
+const navMenu2 = document.querySelector(".nav-menu2");
+
+
+
+navButton2.addEventListener("click", function(){
+    navMenu2.classList.toggle("show");
+})
+
+
+
+
