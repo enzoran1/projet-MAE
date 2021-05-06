e6ad8e8c5a1c2c9a932eee2fae9f747c5abd0336
Martin <martin.aubertin@gmail.com>
2021-05-06 16:10:19 +0200

    Ajouts de nouvelles fonctionnalités - Réecriture d'url en place supprimant l'extension des pages dans la barre de l'url - création des règles de base htaccess (index, errors...) Maintenant, il n'est plus nécessaire dans les href de préciser l'extension compte tenu du fichier .htaccess

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ include 'inc/inc_top.php';
 <div class="ban">
     <div class="btn1">
         <div class="btn">
-            <a href="pages/inscription.php">Inscription</a>
-            <a href="pages/connexion.php">Connexion</a>
+            <a href="pages/inscription">Inscription</a>
+            <a href="pages/connexion">Connexion</a>
         </div>
     </div>
 </div>
