80631005f632098e6a063449d6b1a4b0a4af082a
Martin <martin.aubertin@gmail.com>
2021-05-06 09:45:41 +0200

    Résolution du problème sur la bdd Impossible de se connecter via localhost sur le dsn distant ovh. Nous avons du important la bdd sur le serveur local Wampp pour pouvoir l'exploiter.

--- ifaco/pages/cobdd.php
+++ ifaco/pages/cobdd.php
@@ -2,11 +2,11 @@
 
 
 try {
-    $bdd = new PDO('mysql:host=141.94.17.99:3306;dbname=projetifa;charset=utf8mb4', 'root', 'azerty');
+    $bdd = new PDO('mysql:host=localhost;dbname=projetifa', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (Exception $e) {
     die('erreur :' . $e->getMessage());
-}
+} 
 
 ?>
