6daffc1c023b7ff921ce2f3ab900a2a8c3af122a
Enzo Franceschini <enzo.franceschinidev@gmail.com>
2021-05-06 21:54:01 +0200

    page home fini il reste jsute a changer la photo

--- index.php
+++ index.php
@@ -5,10 +5,16 @@ include 'inc/inc_top.php';
 ?>
 
 <div class="ban">
-    <div class="btn1">
-        <div class="btn">
-            <a href="pages/inscription">Inscription</a>
-            <a href="pages/connexion">Connexion</a>
+    <div class="test">
+        <div class="ban-titre">
+            <h1>Alumn'IFA</h1>
+            <p>retrouvez vos anciens collégues</p>
+        </div>
+        <div class="btn1">
+            <div class="btn">
+                <a href="pages/inscription">Inscription</a>
+                <a href="pages/connexion">Connexion</a>
+            </div>
         </div>
     </div>
 </div>
