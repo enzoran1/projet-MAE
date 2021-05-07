447125e3a3cdd66c9fec148ac2e82756ff3bc8fa
Enzo Franceschini <enzo.franceschinidev@gmail.com>
2021-05-07 09:14:06 +0200

    nouveau boutons plus moderne plus responsive version mobile tablette web

--- index.php
+++ index.php
@@ -5,16 +5,15 @@ include 'inc/inc_top.php';
 ?>
 
 <div class="ban">
-    <div class="test">
-        <div class="ban-titre">
-            <h1>Alumn'IFA</h1>
-            <p>retrouvez vos anciens collégues</p>
-        </div>
-        <div class="btn1">
-            <div class="btn">
-                <a href="pages/inscription">Inscription</a>
-                <a href="pages/connexion">Connexion</a>
-            </div>
+
+    <div class="ban-titre">
+        <h1>Alumn'IFA</h1>
+        <p>retrouvez vos anciens collégues</p>
+    </div>
+    <div class="btn1">
+        <div class="btn">
+            <a href="pages/inscription">Inscription</a>
+            <a href="pages/connexion">Connexion</a>
         </div>
     </div>
 </div>
