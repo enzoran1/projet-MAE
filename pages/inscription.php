4060b73404c84f009c8fe9664e265d94c2222df8
Martin <martin.aubertin@gmail.com>
2021-05-11 15:10:07 +0200

    Modification de la partie back
        Plusieurs modifications ont été apportés dans inscription :
            * J'ai retiré le crytage ARGON car il fait trop de caractères et dépasser la limite, on pourra l'optimiser par la suite
            * J'ai retiré le filter_var, qui n'est pas une condition correct pour lancer la requête d'inscription
            * Il est maintenant possible d'ajouter un utilisateur via l'onglet inscription, une seconde requête vérifie si l'email existe déjà pour empêcher le multi-compte
    
        RESTE A FAIRE :
            * Erreur sur le header location ligne 47 ? ...
            * Optimiser le code lorsque nous aurons déjà avancé

--- pages/inscription.php
+++ pages/inscription.php
@@ -8,28 +8,47 @@ $requete->execute();
 $situations = $requete->fetchAll(PDO::FETCH_ASSOC);
 
 
+if (!empty($_POST['submit'])) 
+{ // On vérifie que le submit est lancé et que tous les champs sont remlis 
 
+    if (!empty($_POST['email']) 
+    && !empty($_POST['email']) 
+    && !empty($_POST['password']) 
+    && !empty($_POST['tel'])) {
 
-if (!empty($_POST['submit'])) {
+        echo 'ok <br>';
 
-    if (isset($_POST['email'], $_POST['password'], $_POST['tel']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['tel'])) {
+        // créer une requête permettant de comparer tous les mails avec celui entré (rowCount>0) 
 
-        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            die("l'adresse email est incorrecte");
-        }
         $email = $_POST['email'];
-        $password = password_hash($_POST['password'], PASSWORD_ARGON2ID);
-        $tel = strip_tags($_POST['tel']);
-    }
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $tel = $_POST['tel'];
 
-    $requete = $bdd->prepare('INSERT INTO user(mail, mdp, telephone) VALUES(?,?,?)');
-    $requete->execute(array($email, $password, $tel));
+        $requestEmailExist = $bdd->prepare("SELECT * FROM user WHERE email ='".$email."'");
+        $requestEmailExist->execute();;
+        $count = $requestEmailExist->rowCount(); // renvoi 0 si l'user n'existe pas... 1 s'il existe
+        
+        echo 'Vérification de l\'email... <br>';
 
-    if (isset($_POST['situation']) && $_POST['situation'] == 1) {
-        header('Location: inseleves.php');
-    }
-    
-    var_dump($_POST);
+        if($count > 0) 
+        { 
+            echo 'L\'email que vous avez utilisé existe déjà.';
+            die;
+        } 
+        else
+        { 
+            // dans le cas ou count est égal à 0
+            $requete = $bdd->prepare('INSERT INTO user(email, mdp, telephone) VALUES(?,?,?)');
+            $requete->execute(array($email, $password, $tel));
+            echo 'Votre compte a été créee';
+
+            if (isset($_POST['situation'])) 
+            {
+                header('Location: ./inseleves.php');
+            }
+        }
+
+    }   
 }
 
 
@@ -69,7 +88,9 @@ if (!empty($_POST['submit'])) {
     </div>
 
 
-    <button type="submit">envoyer</button>
+    <button type="submit" name="submit" value='submit'>Envoyer</button>
+
+    <?php var_dump($_POST); ?>
 
 
 </form>
