947bea7941e1cc3beda1e9152edbdc633ddeec99
Martin <martin.aubertin@gmail.com>
2021-06-14 16:41:32 +0200

    view association debug

--- templates/association/index.php
+++ templates/association/index.php
@@ -9,15 +9,15 @@
     <div class="carousel-inner">
         <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
         <div class="carousel-item">
-            <img src="/images/logo_GEN_H.png">
+            <img src="/image/logo_GEN_H.png">
         </div>
         <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
         <div class="carousel-item">
-            <img src="/images/logo-region.png">
+            <img src="/image/logo-region.png">
         </div>
         <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
         <div class="carousel-item">
-            <img src="/images/metz-met-ok.png">
+            <img src="/image/metz-met-ok.png">
         </div>
         <label for="carousel-3" class="carousel-control prev control-1">‹</label>
         <label for="carousel-2" class="carousel-control next control-1">›</label>
